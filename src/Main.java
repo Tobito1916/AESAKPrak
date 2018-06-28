324b7a2e9911ef030eb1a781a5e99c2c65cd308b
vargr <jonas.hoesl@gmx.de>
2018-06-28 12:04:19 +0200

    AES funktionsfähig

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,28 @@
 import java.io.UnsupportedEncodingException;
 import java.security.*;
+import java.util.Base64;
 import javax.crypto.*;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class Main {
-    public static void main(String args[]) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException {
-        // Schlüssel erzeugen
-        SecretKey secretKey = KeyGenerator.getInstance("2b7e151628aed2a6abf7158809cf4f3c").generateKey();
+    public static void main(String args[]) throws NoSuchAlgorithmException, NoSuchPaddingException, InvalidKeyException, BadPaddingException, IllegalBlockSizeException, UnsupportedEncodingException, InvalidAlgorithmParameterException {
+        String s1= "";
+        String key="2b7e151628aed2a6abf7158809cf4f3c";
+        String value="3243f6a8885a308d313198a2e0370734";
+        IvParameterSpec iv = new IvParameterSpec(value.getBytes("UTF-8"));
+        SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
 
-// Verfahren wählen
-        Cipher cipher = Cipher.getInstance("AES-128");
-        cipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        Cipher cipher = Cipher.getInstance("AES/ECB/NoPadding");
+        cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
 
-// Umwandlung des Strings nach Bytes basierend auf UTF-8
-        byte[] utf8Bytes = "Zu verschlüsselnder String".getBytes("UTF8");
 
-// Verschlüsselung
-        byte[] encryptedBytes = cipher.doFinal(utf8Bytes);
+        byte[] encrypted = cipher.doFinal(value.getBytes());
+       for (int i = 0; i<encrypted.length;i++){
+           s1 = s1 + String.format("%8s", Integer.toBinaryString(encrypted[i] & 0xFF)).replace(' ', '0');
+        }
+        System.out.println(s1);
 
-// Base64 encoding um wieder einen String zu bekommen
-        String encryptedString = java.util.Base64.getEncoder().encodeToString(encryptedBytes);
-        System.out.println(encryptedString);
     }
 
 }
